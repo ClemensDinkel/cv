a5fda3fb5efcaecefc6c028b45ce61aa2304396e
Clemens Dinkel <cle.dinkel@gmail.com>
2024-02-02 20:35:56 +0100

    fill with first data

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "oxford-blue": "#10253F",
+        "non-photo-blue": "#93CDDD",
+      },
+      fontSize: {
+        sm: ["16px", "30px"]
+      }
+    },
   },
   plugins: [],
 }
