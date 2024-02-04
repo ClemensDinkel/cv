e9da376dc9ba4b303276dca14dd82bd9b42ca0f2
Clemens Dinkel <cle.dinkel@gmail.com>
2024-02-04 19:19:41 +0100

    delete unimportant data

--- generatePdf.js
+++ generatePdf.js
@@ -4,5 +4,7 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto("file:///D:/Projects/cv/cv-de.html")
   await page.pdf({ path: "cv-de.pdf", format: "A4", printBackground: true });
+  await page.goto("file:///D:/Projects/cv/cv-en.html")
+  await page.pdf({ path: "cv-en.pdf", format: "A4", printBackground: true });
   await browser.close();
 })();
