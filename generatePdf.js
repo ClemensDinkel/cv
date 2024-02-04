4320fb2aa83db32915107bc7460302ea9987ca45
Clemens Dinkel <cle.dinkel@gmail.com>
2024-02-04 16:52:00 +0100

    merge pages and use page break

--- generatePdf.js
+++ generatePdf.js
@@ -2,9 +2,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("file:///D:/Projects/cv/page1-de.html")
-  await page.pdf({ path: "page1.pdf", format: "A4", printBackground: true });
-  await page.goto("file:///D:/Projects/cv/page2-de.html")
-  await page.pdf({ path: "page2.pdf", format: "A4", printBackground: true });
+  await page.goto("file:///D:/Projects/cv/cv-de.html")
+  await page.pdf({ path: "cv-de.pdf", format: "A4", printBackground: true });
   await browser.close();
 })();
